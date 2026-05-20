5cf4a96a54e3e63210fcf408bcf651b0a38f6a90
kali <kali@kali.kali>
2026-05-20 12:59:59 +0530

    vercel json

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
   
   connectionTimeout: 10000,
   greetingTimeout: 10000,
